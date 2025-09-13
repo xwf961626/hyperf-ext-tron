9026486f5abe8ca93b8ad32604ce3eeb1b43de6f
wenfeng <wenfeng@git.logon8.com>
2025-09-13 10:57:28 +0800

    addLimitAddressRoutes

--- src/TronRouter.php
+++ src/TronRouter.php
@@ -5,6 +5,7 @@ namespace William\HyperfExtTron;
 use Hyperf\HttpServer\Contract\RequestInterface;
 use Hyperf\HttpServer\Contract\ResponseInterface;
 use Hyperf\HttpServer\Router\Router;
+use William\HyperfExtTron\Controller\LimitAddressController;
 use William\HyperfExtTron\Controller\UserResourceAddressController;
 use William\HyperfExtTron\Tron\ApiController;
 use William\HyperfExtTron\Tron\Energy\EnergyApiFactory;
@@ -32,5 +33,15 @@ class TronRouter
         Router::addRoute(['GET', 'POST'], '/api/callback/{name}', function (string $name, RequestInterface $request, ResponseInterface $response) {
             return make(EnergyApiFactory::class)->handleApiCallback($name, $request, $response);
         });
+    }
+    
+    public static function addLimitAddressRoutes(): void
+    {
+        Router::get('bandwidth/addresses', [LimitAddressController::class, 'addressList']);
+        Router::post('bandwidth/addresses', [LimitAddressController::class, 'addAddress']);
+        Router::put('bandwidth/addresses/{id}', [LimitAddressController::class, 'editAddress']);
+        Router::delete('bandwidth/addresses/{id}', [LimitAddressController::class, 'deleteAddress']);
+        Router::get('bandwidth/logs', [LimitAddressController::class, 'getLogs']);
+        Router::put('bandwidth/logs/{id}/retry_recycle', [LimitAddressController::class, 'retryRecycle']);
     }
 }
